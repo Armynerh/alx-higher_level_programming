d0a34ce92752c1b14dcfc67694d514312d4307f0
Armynerh <aimenah@outlook.com>
2022-08-10 12:19:48 +0100

    some tasks

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 --create a table
 CREATE TABLE IF NOT EXISTS `first_table` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,DEFAULT 0
   `name` varchar(255) NOT NULL DEFAULT '')
