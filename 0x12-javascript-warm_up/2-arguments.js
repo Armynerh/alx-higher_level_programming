e076294a10976803d8211c0ee27d5160ecf52b2c
Armynerh <aimenah@outlook.com>
2022-09-06 02:07:28 +0100

    Javascript Tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
+const myArgs = process.argv.slice(2);
+if(myArgs.length == 0){
+  console.log('No argument');
+}
+else if(myArgs.length == 1){
+  console.log('Argument Found');
+}
+else {
+  console.log('Argument Found');
+}
 
-import { argv } from 'node:process';
-
-// print process.argv
-argv.forEach((val) => {
-  if (val === null) {
-    console.log('No argument');
-  } else if (val.length === 1) {
-    console.log('No argument');
-  } else {
-    console.log('Arguments found');
-  }
-});
